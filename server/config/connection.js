50be00c1bace5565b087664bf3fb3e5b86dbde01
bybymaxy <141274790+bybymaxy@users.noreply.github.com>
2024-03-13 20:24:49 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/storyweaver-ai', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
